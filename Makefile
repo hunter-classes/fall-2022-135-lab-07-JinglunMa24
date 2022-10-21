b13af1073f3222b90dc436817c4eaba3ea794a9e
Jinglun Ma <2461447099ma@gmail.com>
2022-10-20 21:45:11 -0400

    added

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
-main: main.o unindent.o
-	g++ -o main main.o unindent.o
+main: main.o unindent.o indent.o
+	g++ -o main main.o unindent.o indent.o
 
 main.o: main.cpp unindent.h
 
 unindent.o: unindent.cpp unindent.h
 
+indent.o: indent.cpp indent.h
 
 clean:
-	rm -f main.o unindent.o
+	rm -f main.o unindent.o indent.o
--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
+#include <fstream>
 #include "unindent.h"
+#include "indent.h"
 
 int main() {
-	std::string line = "int main(){           ";
 
+	//Task A
+	std::string line = "       int x = 1;  ";
 	std::cout << '"' << line << '"' << std::endl << '"' << removeLeadingSpaces(line) << '"' << std::endl;
-	
+
+	//Read bad-code.cpp
+	std::string outputA, outputB;
+	std::ifstream badF("bad-code.cpp");
+	if (badF.fail()) {
+		std::cout << "Failed to open file" << std::endl;
+		return 1;
+	}
+
+	// fix bad-code.cpp
+	outputB = indentation(badF);
+	std::cout << "\nFixed bad-code.cpp:\n" << outputB << std::endl;
+
 
 	return 0;
 }
